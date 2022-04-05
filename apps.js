b06d003afbac6a60513868bac33c3afd011a8e54
Christopher Foose <chrisfoose@gmail.com>
2022-04-04 17:46:14 -0700

    first commit

---
+++ apps.js
@@ -0,0 +1,44 @@
+console.log('Connected!');
+
+/* 
+fetch('https://type.fit/api/quotes')
+    .then(response => response.json())
+    .then(json => json)
+*/
+
+//let displayQuote = '';
+let quoteButton = document.getElementById('quoteButton');
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+
+/*
+async function getQuote(event) {
+    randomNumber = Math.floor(Math.random() * json.length);
+    console.log(randomNumber);
+    return randomNumber;
+
+}; */
+
+
+
+const newQuote = async (event) => {     
+    try { 
+
+    
+    const url = 'https://type.fit/api/quotes';
+
+    const response = await fetch(url);
+    if (response.ok) {
+        let jsonResponse = await response.json();
+        num = Math.floor(Math.random() * 1600);
+
+        quote.innerHTML = jsonResponse[num].text;
+        author.innerHTML = jsonResponse[num].author;
+        
+    }
+} catch (error) {
+    console.log(error);
+}}
+   
+
+quoteButton.addEventListener('click', newQuote);
